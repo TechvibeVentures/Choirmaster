fb6ac54c46c46bec512bfde0a7b992cf459993fd
Johannes Röhrenbach <mail@roehrenbach.org>
2026-02-11 21:21:54 +0100

    Remove legacy projects routes

--- app/(app)/projects/[projectId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function ProjectDetailPage({
-  params
-}: {
-  params: { projectId: string };
-}) {
-  redirect(`/calendar/${params.projectId}`);
-}
